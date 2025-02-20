3d82f7e7bf2fd2e836c71063c92ca06331339da7
mohanson <mohanson@outlook.com>
2025-02-20 15:56:54 +0800

    Optimizing eddsa

--- pxsol/eddsa.py
+++ pxsol/eddsa.py
@@ -27,27 +27,28 @@ def pt_decode(pt: bytearray) -> pxsol.ed25519.Pt:
     # significant bit of the x-coordinate and denote this value x_0. The y-coordinate is recovered simply by clearing
     # this bit. If the resulting value is >= p, decoding fails.
     uint = int.from_bytes(pt, 'little')
-    sign = uint >> 255
+    bit0 = uint >> 255
     yint = uint & ((1 << 255) - 1)
     assert yint < pxsol.ed25519.P
     # To recover the x-coordinate, the curve equation implies x^2 = (y^2 - 1) / (d y^2 + 1) (mod p). The denominator is
     # always non-zero mod p.
     y = pxsol.ed25519.Fq(yint)
-    x_x = (y * y - pxsol.ed25519.Fq(1)) / (pxsol.ed25519.D * y * y + pxsol.ed25519.Fq(1))
+    u = y * y - pxsol.ed25519.Fq(1)
+    v = pxsol.ed25519.D * y * y + pxsol.ed25519.Fq(1)
+    w = u / v
     # To compute the square root of (u/v), the first step is to compute the candidate root x = (u/v)^((p+3)/8).
-    x = x_x ** ((pxsol.ed25519.P + 3) // 8)
+    x = w ** ((pxsol.ed25519.P + 3) // 8)
     # Again, there are three cases:
-    # 1. If v x^2 = u (mod p), x is a square root.
+    # 1. If v x^2 = +u (mod p), x is a square root.
     # 2. If v x^2 = -u (mod p), set x <-- x * 2^((p-1)/4), which is a square root.
     # 3. Otherwise, no square root exists for modulo p, and decoding fails.
-    if x*x != x_x:
+    if x*x != w:
         x = x * pxsol.ed25519.Fq(2) ** ((pxsol.ed25519.P - 1) // 4)
-        assert x*x == x_x
+        assert x*x == w
     # Finally, use the x_0 bit to select the right square root. If x = 0, and x_0 = 1, decoding fails. Otherwise, if
     # x_0 != x mod 2, set x <-- p - x.  Return the decoded point (x,y).
-    if x == pxsol.ed25519.Fq(0):
-        assert not sign
-    if x.x & 1 != sign:
+    assert x != pxsol.ed25519.Fq(0) or not bit0
+    if x.x & 1 != bit0:
         x = -x
     return pxsol.ed25519.Pt(x, y)
 
@@ -55,18 +56,20 @@ def pt_decode(pt: bytearray) -> pxsol.ed25519.Pt:
 def pt_exists(pt: bytearray) -> bool:
     # Tests whether a point is on ed25519 curve.
     uint = int.from_bytes(pt, 'little')
-    sign = uint >> 255
+    bit0 = uint >> 255
     yint = uint & ((1 << 255) - 1)
     if yint >= pxsol.ed25519.P:
         return False
     y = pxsol.ed25519.Fq(yint)
-    x_x = (y * y - pxsol.ed25519.Fq(1)) / (pxsol.ed25519.D * y * y + pxsol.ed25519.Fq(1))
-    x = x_x ** ((pxsol.ed25519.P + 3) // 8)
-    if x*x != x_x:
+    u = y * y - pxsol.ed25519.Fq(1)
+    v = pxsol.ed25519.D * y * y + pxsol.ed25519.Fq(1)
+    w = u / v
+    x = w ** ((pxsol.ed25519.P + 3) // 8)
+    if x*x != w:
         x = x * pxsol.ed25519.Fq(2) ** ((pxsol.ed25519.P - 1) // 4)
-    if x*x != x_x:
+    if x*x != w:
         return False
-    if x == pxsol.ed25519.Fq(0) and sign:
+    if x == pxsol.ed25519.Fq(0) and bit0:
         return False
     return True
 
@@ -90,26 +93,25 @@ def sign(prikey: bytearray, m: bytearray) -> bytearray:
     a &= (1 << 254) - 8
     a |= (1 << 254)
     a = pxsol.ed25519.Fr(a)
+    A = pxsol.ed25519.G * a
+    pubkey = pt_encode(A)
     prefix = h[32:]
-    A = pt_encode(pxsol.ed25519.G * a)
     r = pxsol.ed25519.Fr(int.from_bytes(hash(prefix + m), 'little'))
     R = pxsol.ed25519.G * r
-    Rs = pt_encode(R)
-    h = pxsol.ed25519.Fr(int.from_bytes(hash(Rs + A + m), 'little'))
-    s = r + h * a
-    return Rs + bytearray(s.x.to_bytes(32, 'little'))
+    digest = pt_encode(R)
+    h = pxsol.ed25519.Fr(int.from_bytes(hash(digest + pubkey + m), 'little'))
+    s = r + a * h
+    return digest + bytearray(s.x.to_bytes(32, 'little'))
 
 
-def verify(pubkey: bytearray, m: bytearray, sig: bytearray) -> bool:
+def verify(pubkey: bytearray, m: bytearray, v: bytearray) -> bool:
     # Verify a signature on a message using public key.
     # See https://datatracker.ietf.org/doc/html/rfc8032#section-5.1.7
     assert len(pubkey) == 32
-    assert len(sig) == 64
+    assert len(v) == 64
     A = pt_decode(pubkey)
-    Rs = sig[:32]
-    R = pt_decode(Rs)
-    s = pxsol.ed25519.Fr(int.from_bytes(sig[32:], 'little'))
-    h = pxsol.ed25519.Fr(int.from_bytes(hash(Rs + pubkey + m), 'little'))
-    sB = pxsol.ed25519.G * s
-    hA = A * h
-    return sB == R + hA
+    digest = v[:32]
+    R = pt_decode(digest)
+    s = pxsol.ed25519.Fr(int.from_bytes(v[32:], 'little'))
+    h = pxsol.ed25519.Fr(int.from_bytes(hash(digest + pubkey + m), 'little'))
+    return pxsol.ed25519.G * s == R + A * h
